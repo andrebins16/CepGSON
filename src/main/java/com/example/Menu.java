ef01b9f20ce0268cac0714d0ae9b6acdf0cce110
bins <a.bins@edu.pucrs.br>
2023-04-03 00:03:21 -0300

    vFinal3

--- src/main/java/com/example/Menu.java
+++ src/main/java/com/example/Menu.java
@@ -5,7 +5,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest; 
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Scanner;
 import static java.time.temporal.ChronoUnit.MINUTES;
 import com.google.gson.Gson;
